16d95f8ea08d94287e864288c6942dc2875fb6ba
lestherxm <lestherxm@gmail.com>
2023-02-12 17:16:21 -0600

    uploading project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# image_slider
+# Image Slider
+
+This project checks if there are no more images in the path images set, in which case it resets the current index to show the initial image of the path images set, thus conceptually creating a circular queue path.
+
+### [Click here](https://lestherxm.github.io/image_slider/) to see the demo.
+### Author: [`Lesther Xitumul Manuel`](https://andoti.com)
---
+++ src/app.js
@@ -0,0 +1,40 @@
+const btnBackward = document.querySelector('.btn-backward');
+const btnForward = document.querySelector('.btn-forward');
+const currentImage = document.querySelector('.current-image');
+
+const images = [
+    '../src/img/slider/01.jpg',
+    '../src/img/slider/02.jpg',
+    '../src/img/slider/03.jpg',
+    '../src/img/slider/04.jpg',
+    '../src/img/slider/05.jpg',
+    '../src/img/slider/06.jpg',
+];
+// setting a default img index
+let currentIndex = 0;
+
+function imgForward() {
+    currentIndex++;
+    if(currentIndex >= images.length){
+        currentIndex = 0;
+    }
+    showImage();
+}
+
+function imgBackward() {
+        currentIndex--;
+        if(currentIndex < 0){
+            currentIndex = images.length - 1;
+        }
+        showImage();
+}
+
+function showImage() {
+    currentImage.setAttribute('src', images[currentIndex]);
+}
+
+btnForward.addEventListener('click', imgForward);
+btnBackward.addEventListener('click', imgBackward);
+
+
+showImage(); // show the image in position zero by default
